94dc46a561c35988425746f06b07edd01a34c5bd
Iratxe Garrido <iratxe.ggarrido@gmail.com>
2023-12-13 16:30:34 +0100

    Header & Navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,21 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
 
+/* Menu Show */
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+  })
+}
+
+/* Menu Hide */
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
@@ -10,13 +26,13 @@
 /*=============== EMAIL JS ===============*/
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
-/*=============== DARK LIGHT THEME ===============*/ 
+/*=============== DARK LIGHT THEME ===============*/
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
